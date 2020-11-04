586db963d8e6444d3056fc90da954b7ecc3a1ed7
Mark Yacoub <68869747+myacoub005@users.noreply.github.com>
2020-11-03 18:35:49 -0800

    Update count_up.py

--- count_up.py
+++ count_up.py
@@ -77,9 +77,9 @@ with gzip.open(gff, "rt") as fh:
         if row[0].startswith("#"):
             continue
         if row[2]=="gene":
-        genecount += 1
-        lengths = int(row[4])-int(row[3])
-        gene_lengths.append(lengths)
+            genecount += 1
+            lengths = int(row[4])-int(row[3])
+            gene_lengths.append(lengths)
 coding_length = sum(gene_lengths) #this will add all the gene lengths together, giving us total coding length
 print("there are",genecount,"in the E. coli genome")
 print("the total coding length of the genome is",coding_length)
